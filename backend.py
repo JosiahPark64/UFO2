5076f502d9e40075302eb82b5ed2cdd6b597cacc
Ryan-L-N <thenevilletrove@gmail.com>
2025-08-13 11:25:57 -0400

    Fixed some findings

--- backend.py
+++ backend.py
@@ -6,10 +6,7 @@ ip = "0.0.0.0"
 port = 8000
 server_address = (ip, port)
 
-# Define and init a variable called "api" using the classes we imported. 
-api = HTTPServer(
-    server_address=server_address, RequestHandlerClass=SimpleHTTPRequestHandler
-)
+api = HTTPServer(server_address=server_address, RequestHandlerClass=SimpleHTTPRequestHandler)
 
 # Start the HTTP server.
 print("Listening for HTTP requests...")
